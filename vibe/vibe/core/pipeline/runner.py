abf1c9cf3b37081e80e2f8f49236c6de1e98a52e
Ademfcan <ademfcan@gmail.com>
2025-09-03 23:28:38 -0700

    llm format

--- vibe/vibe/core/pipeline/runner.py
+++ vibe/vibe/core/pipeline/runner.py
@@ -1,10 +1,14 @@
 from typing import List
+
+from vibe.core.data import models
 from ..git_interface.interface import GitInterface
 from ..diff_extractor.interface import DiffExtractorInterface
 from ..chunker.interface import ChunkerInterface
 from ..grouper.interface import GrouperInterface
 from ..commiter.interface import CommitterInterface
 from ..data.models import DiffChunk, ChunkGroup, CommitResult
+
+import inquirer
 
 class AIGitPipeline:
     def __init__(
@@ -21,24 +25,41 @@ class AIGitPipeline:
         self.grouper: GrouperInterface = grouper
         self.committer: CommitterInterface = committer
 
-    def run(self) -> List[CommitResult]:
+    def run(self, target : str) -> List[CommitResult]:
         # Step 0: clean working area
-        self.git.reset()
+
+        unstage = inquirer.confirm("Would you like to unstage all changes?", default=False)
+        
+        if unstage:
+            self.git.reset()
+
+        track = inquirer.confirm(f"Would you like to track all untracked files within {target}?", default=False)
+        
+        if track:
+            self.git.track_untracked(target)
 
         # Step 1: extract diff
-        raw_diff: List[DiffChunk] = self.diff_extractor.extract_diff(self.git)
+        raw_diff: List[DiffChunk] = self.git.get_working_diff(target)
+
+        for chunk in raw_diff:
+            w_move = models.detect_moves(chunk.ai_content)
+            w_replacements = models.detect_replacements(w_move)
+            print(models.format_diff_for_llm(chunk.file_path, w_replacements))
+
+
+
 
         # Step 2: split into chunks
-        chunks: List[DiffChunk] = self.chunker.chunk(raw_diff)
+        # chunks: List[DiffChunk] = self.chunker.chunk(raw_diff)
 
-        # Step 3: group chunks
-        grouped: List[ChunkGroup] = self.grouper.group_chunks(chunks)
+        # # Step 3: group chunks
+        # grouped: List[ChunkGroup] = self.grouper.group_chunks(chunks)
 
-        # Step 4: commit each group
-        results: List[CommitResult] = []
-        for group in grouped:
-            commit_result = self.committer.create_commit(group, self.git)
-            results.append(commit_result)
-            print(f"Created commit {commit_result.commit_hash} for group {group.group_id}")
+        # # Step 4: commit each group
+        # results: List[CommitResult] = []
+        # for group in grouped:
+        #     commit_result = self.committer.create_commit(group, self.git)
+        #     results.append(commit_result)
+        #     print(f"Created commit {commit_result.commit_hash} for group {group.group_id}")
 
-        return results
+        # return results
