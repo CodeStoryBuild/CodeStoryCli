8ae1b62221713c74933dd8ef5d2099e332731ed0
AdemCan <ademfcan@gmail.com>
2025-12-02 19:42:02 -0800

    remove old liscence

--- codestory/tests/unit/core/git_interface/SubprocessGitInterface_test.py
+++ codestory/tests/unit/core/git_interface/SubprocessGitInterface_test.py
@@ -1,25 +1,3 @@
-# -----------------------------------------------------------------------------
-# codestory - Dual Licensed Software
-# Copyright (c) 2025 Adem Can
-#
-# This file is part of codestory.
-#
-# codestory is available under a dual-license:
-#   1. AGPLv3 (Affero General Public License v3)
-#      - See LICENSE.txt and LICENSE-AGPL.txt
-#      - Online: https://www.gnu.org/licenses/agpl-3.0.html
-#
-#   2. Commercial License
-#      - For proprietary or revenue-generating use,
-#        including SaaS, embedding in closed-source software,
-#        or avoiding AGPL obligations.
-#      - See LICENSE.txt and COMMERCIAL-LICENSE.txt
-#      - Contact: ademfcan@gmail.com
-#
-# By using this file, you agree to the terms of one of the two licenses above.
-# -----------------------------------------------------------------------------
-
-
 import subprocess
 from pathlib import Path
 from unittest.mock import Mock, patch
