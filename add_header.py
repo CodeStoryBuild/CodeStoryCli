bc0e31430754516978d5210947d1df35565deaff
AdemCan <ademfcan@gmail.com>
2026-01-03 04:03:06 -0800

    Convert test license headers to commented blocks

--- add_header.py
+++ add_header.py
@@ -6,27 +6,25 @@ import re
 
 # --- Configuration ---
 
-LICENSE_HEADER = '''
-"""
------------------------------------------------------------------------------
-/*
- * Copyright (C) 2025 CodeStory
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; Version 2.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, you can contact us at support@codestory.build
- */
------------------------------------------------------------------------------
-"""
-'''.strip()  # .strip() removes leading/trailing whitespace/newlines from the triple quotes
+LICENSE_HEADER = """
+# -----------------------------------------------------------------------------
+# /*
+#  * Copyright (C) 2025 CodeStory
+#  *
+#  * This program is free software; you can redistribute it and/or modify
+#  * it under the terms of the GNU General Public License as published by
+#  * the Free Software Foundation; Version 2.
+#  *
+#  * This program is distributed in the hope that it will be useful,
+#  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  * GNU General Public License for more details.
+#  *
+#  * You should have received a copy of the GNU General Public License
+#  * along with this program; if not, you can contact us at support@codestory.build
+#  */
+# -----------------------------------------------------------------------------
+""".strip()  # .strip() removes leading/trailing whitespace/newlines from the triple quotes
 
 
 def add_license_header(root_dir):
