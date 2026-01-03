065b77f433a26f8aa8faca077b98d51615a775f4
AdemCan <ademfcan@gmail.com>
2026-01-03 04:02:38 -0800

    Fix GPL header punctuation in core module files

--- add_header.py
+++ add_header.py
@@ -22,7 +22,7 @@ LICENSE_HEADER = '''
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, you can contact us at support@codestory.build.
+ * along with this program; if not, you can contact us at support@codestory.build
  */
 -----------------------------------------------------------------------------
 """
