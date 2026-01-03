82ea24c23769c4b977257bc51e3301b8c4aea312
AdemCan <ademfcan@gmail.com>
2026-01-03 03:59:59 -0800

    Insert blank lines before header and license sections

--- dslate/dslate/pipelines/commit_pipeline.py
+++ dslate/dslate/pipelines/commit_pipeline.py
@@ -15,16 +15,27 @@
 #   1. AGPLv3 (Affero General Public License v3)
 
 #      - See LICENSE.txt and LICENSE-AGPL.txt
+
 #      - Online: https://www.gnu.org/licenses/agpl-3.0.html
+
 #
+
 #   2. Commercial License
+
 #      - For proprietary or revenue-generating use,
+
 #        including SaaS, embedding in closed-source software,
+
 #        or avoiding AGPL obligations.
+
 #      - See LICENSE.txt and COMMERCIAL-LICENSE.txt
+
 #      - Contact: ademfcan@gmail.com
+
 #
+
 # By using this file, you agree to the terms of one of the two licenses above.
+
 # -----------------------------------------------------------------------------
 
 
