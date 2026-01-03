a0b04c445e1a755280922802bdb4c6df408d1156
AdemCan <ademfcan@gmail.com>
2026-01-03 04:02:38 -0800

    Fix GPL header punctuation in chunking and diff generator files

--- codestory/codestory/core/data/chunk.py
+++ codestory/codestory/core/data/chunk.py
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, you can contact us at support@codestory.build.
+ * along with this program; if not, you can contact us at support@codestory.build
  */
 -----------------------------------------------------------------------------
 """
