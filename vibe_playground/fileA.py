659c973f6428ce833e325176bf648a460b450643
AdemCan <ademfcan@gmail.com>
2026-01-03 03:55:45 -0800

    Remove preprocessor, add Greeter class and farewell functions to examples

--- vibe_playground/fileA.py
+++ vibe_playground/fileA.py
@@ -2,3 +2,13 @@ def calculate():
     return 42
 
 print(calculate())
+
+class Greeter:
+    def say_hello(self):
+        self.aa = 1 
+
+    def say_bye(self):
+        print(20)
+
+def farewell():
+    print("Goodbye")
--- vibe_playground/fileB.py
+++ vibe_playground/fileB.py
@@ -2,3 +2,11 @@ def calculate(multiplier):
     return 42 * multiplier
 
 print(calculate(2))
+
+class Greeter:
+    def say_hello(self):
+        # A friendly greeting
+        print("Hello, friend")
+
+def farewell():
+    print("Goodbye, friend")
